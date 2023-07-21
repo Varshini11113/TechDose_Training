83adbcd8675a258bd27f8b9e5b7acaeabef3811f
Varshini11113 <varshini.m2021csec@sece.ac.in>
2023-07-21 09:52:00 +0530

    Quick sort added in chapter 4

---
+++ TechDose Chapter3/PrimeFactorization.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int n = 19;
+
+int main(){
+    createSeive(n);
+    while(n != 1){
+        
+    }
+}
---
+++ TechDose Chapter4/QuickSort.cpp
@@ -0,0 +1,22 @@
+//Quicksort algorithm
+int partition(vector<int>&nums, int low, int high){
+    int pivot = nums[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+        while(nums[i] <= pivot && i <= high-1) i++;
+        while(nums[j] > pivot && j >= low+1) j--;
+        if(i < j) swap(nums[i], nums[j]);
+    }
+    swap(nums[j], nums[low]);
+    return j;
+}
+//here the first element is choosen as the pivot element
+
+void quickSort(vector<int>&nums, int low, int high){
+    if(low <= high){
+        int pIdx = partition(nums, low, high);
+        quickSort(nums, low, pIdx-1);
+        quickSort(nums, pIdx+1, high);
+    }
+}

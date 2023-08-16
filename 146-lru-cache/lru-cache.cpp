dae62511b488ad28b5fc0124a36a3f550baf6c5e
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-08-16 15:36:10 +0530

    Time: 408 ms (92.69%) | Memory: 174.1 MB (52.12%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,39 +1,37 @@
 class LRUCache {
 public:
-    list<pair<int,int>> cache;
-    unordered_map<int,list<pair<int,int>>::iterator> mymap; 
-    void refreshPosition(int key,int value)
-        {
-            cache.erase(mymap[key]);
-            cache.push_front(make_pair(key,value));
-            mymap[key] = cache.begin();     
-        }
-    int capacity;
+    list<pair<int,int>>cache;
+    unordered_map<int, list<pair<int,int>>::iterator> map;
+   
+    void refresh(int key, int value){
+        cache.erase(map[key]);
+        cache.push_front(make_pair(key, value));
+        map[key] = cache.begin();
+    }
+     int capacity;
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
     
     int get(int key) {
-        if(mymap.find(key)!=mymap.end())
-        {
-            refreshPosition(key,(*mymap[key]).second);
-            return (*mymap[key]).second;
+        if(map.find(key) != map.end()){
+            refresh(key, (*map[key]).second);
+            return (*map[key]).second;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if(mymap.find(key)!=mymap.end())
-            refreshPosition(key,value);
-        else
-        {
-            cache.push_front(make_pair(key,value));
-            mymap[key] = cache.begin();
-            if(mymap.size()>capacity)
-            {
-                mymap.erase(cache.back().first);
-                cache.pop_back();
-            }
+        if(map.find(key) != map.end()){
+            refresh(key, value);
+        }
+        else{
+            cache.push_front(make_pair(key, value));
+            map[key] = cache.begin();
+        if(map.size() > capacity){
+            map.erase(cache.back().first);
+            cache.pop_back();
+        }
         }
     }
 };

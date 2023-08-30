a477c5b863023b3170edaf0a5f10cfb3e616a3a4
Varshini11113 <varshini.m2021csec@sece.ac.in>
2023-08-30 06:14:40 +0530

    MinHeap and maxHeap code done

--- TechDose Heap Chapter/heap_implementation.cpp
+++ TechDose Heap Chapter/heap_implementation.cpp
@@ -62,3 +62,167 @@ int main() {
     printHeap(heap);
     return 0;
 }
+
+
+//clean code - push(), pop(), heapifyUp, heapifyDown, maxHeap desc..
+
+//Max_Heap
+
+#include <iostream>
+#include <vector>
+
+class MaxHeap {
+private:
+    std::vector<int> heap;
+    void heapifyUp(int index) {
+        int parentIndex = index / 2;
+        while (index > 1 && heap[index] > heap[parentIndex]) {
+            std::swap(heap[index], heap[parentIndex]);
+            index = parentIndex;
+            parentIndex = index / 2;
+        }
+    }
+
+    void heapifyDown(int index) {
+        int n = heap.size() - 1;
+        while (2 * index <= n) {
+            int leftChildIndex = 2 * index;
+            int rightChildIndex = 2 * index + 1;
+            int largestIndex = index;
+
+            if (leftChildIndex <= n && heap[leftChildIndex] > heap[largestIndex]) {
+                largestIndex = leftChildIndex;
+            }
+            if (rightChildIndex <= n && heap[rightChildIndex] > heap[largestIndex]) {
+                largestIndex = rightChildIndex;
+            }
+
+            if (largestIndex != index) {
+                std::swap(heap[index], heap[largestIndex]);
+                index = largestIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+public:
+    void push(int value) {
+        heap.push_back(value);
+        heapifyUp(heap.size() - 1);
+    }
+
+    int pop() {
+        if (heap.size() <= 1) {
+            return -1;  // Heap is empty
+        }
+        int root = heap[1];
+        // std :: cout << root << " "; 
+        heap[1] = heap.back();
+        heap.pop_back();
+        heapifyDown(1);
+        return root;
+    }
+
+    void printDescendingOrder() {
+        while (heap.size() > 1) {
+            std::cout << pop() << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    MaxHeap maxHeap;
+    std::vector<int> elements = {5, 12, 3, 15, 10, 17};
+    maxHeap.push(0);
+    for (int element : elements) {
+        maxHeap.push(element);
+    }
+
+    std::cout << "Elements in descending order:" << std::endl;
+    maxHeap.printDescendingOrder();
+
+    return 0;
+}
+
+//clean code for min_heap only the condtion changes [(x < left) && (x < right)];
+//push(), pop(), asc, heapifyUp, heapifyDown
+
+#include <iostream>
+#include <vector>
+
+class MinHeap {
+private:
+    std::vector<int> heap;
+
+    void heapifyUp(int index) {
+        int parentIndex = index / 2;
+        while (index > 1 && heap[index] < heap[parentIndex]) {
+            std::swap(heap[index], heap[parentIndex]);
+            index = parentIndex;
+            parentIndex = index / 2;
+        }
+    }
+
+    void heapifyDown(int index) {
+        int n = heap.size() - 1;
+        while (2 * index <= n) {
+            int leftChildIndex = 2 * index;
+            int rightChildIndex = 2 * index + 1;
+            int smallestIndex = index;
+
+            if (leftChildIndex <= n && heap[leftChildIndex] < heap[smallestIndex]) {
+                smallestIndex = leftChildIndex;
+            }
+            if (rightChildIndex <= n && heap[rightChildIndex] < heap[smallestIndex]) {
+                smallestIndex = rightChildIndex;
+            }
+
+            if (smallestIndex != index) {
+                std::swap(heap[index], heap[smallestIndex]);
+                index = smallestIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+public:
+    void push(int value) {
+        heap.push_back(value);
+        heapifyUp(heap.size() - 1);
+    }
+
+    int pop() {
+        if (heap.size() <= 1) {
+            return -1;  // Heap is empty
+        }
+        int root = heap[1];
+        heap[1] = heap.back();
+        heap.pop_back();
+        heapifyDown(1);
+        return root;
+    }
+
+    void printAscendingOrder() {
+        while (heap.size() > 1) {
+            std::cout << pop() << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    MinHeap minHeap;
+    std::vector<int> elements = {5, 12, 3, 15, 10, 17};
+    for (int element : elements) {
+        minHeap.push(element);
+    }
+
+    std::cout << "Elements in ascending order:" << std::endl;
+    minHeap.printAscendingOrder();
+
+    return 0;
+}
+

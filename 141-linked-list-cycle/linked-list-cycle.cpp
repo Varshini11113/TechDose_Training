55c296de911c0daf2bc9ffe9b1d210714f066906
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-09-04 21:12:14 +0530

    Update linked-list-cycle.cpp c++ language

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,6 +6,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//C++ language
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -21,24 +23,3 @@ public:
         return false;
     }
 };
-
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     struct ListNode *next;
-//  * };
-//  */
-// bool hasCycle(struct ListNode *head) {
-//     struct ListNode *temp = (struct ListNode*) malloc (sizeof(struct ListNode));
-//     struct ListNode *fast = (struct ListNode*) malloc (sizeof(struct ListNode));
-//     temp = head, fast = head;
-//     while(fast != NULL && fast->next != NULL){
-//         temp = temp->next;
-//         fast = fast->next->next;
-//         if(temp == fast){
-//             return true;
-//         }
-//     }
-//     return false;
-// }

d3cb6de0683e60928a46e3e84d47d9c0e05a6cd6
Varshini11113 <varshini.m2021csec@sece.ac.in>
2023-09-15 10:51:38 +0530

    Tree traversals added (inorder, preorder, postorder)

---
+++ TechDose Tree Chapter/Tree Representation (in, pre, post).cpp
@@ -0,0 +1,52 @@
+// Online C++ compiler to run C++ program online
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+node* nn(int val){
+    struct node *temp = new node;
+    temp->data = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(node *root){
+    if(root == NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(node *root){
+    if(root == NULL) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node *root){
+    if(root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main() {
+    // Write C++ code here
+    // std::cout << "Hello world!";
+    struct node *root = nn(1);
+    root->left = nn(3);
+    root->right = nn(5);
+    root->left->right = nn(11);
+    inorder(root);
+    cout<<"Inorder Traversal"<<"\n";
+    preorder(root);
+    cout<<"Preorder Traversal"<<"\n";
+    postorder(root);
+    cout<<"Postorder Traversal"<<"\n";
+    return 0;
+}

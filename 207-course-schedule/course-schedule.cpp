e86029753014672714f1460cd1373adc4fe92131
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-10-31 11:00:22 +0530

    Time: 14 ms (87.29%) | Memory: 13.4 MB (93.04%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,58 +1,24 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& gn) {
-        // vector<int>adj[n];
-        // int m = gn.size();
-        // vector<int>ind(n,0);
-        // // for(int i = 0; i<m; i++){
-        // //     adj[gn[i].first].push_back(gn[i].second);
-        // //     ind[gn[i].second]++;
-        // // }
-        // // for(auto ele : gn){
-        // //     adj[ele[0]].push_back(ele[1]);
-        // //     ind[ele[1]]++;
-        // // }
-        // for(int i = 0; i<m; i++){
-        //     adj[gn[i][0]].push_back(gn[i][1]);
-        //     ind[gn[i][1]]++;
-        // }
-        // queue<int>q;
-        // for(int i = 0; i<n; i++){
-        //     if(ind[i] == 0) q.push(i);
-        // }
-        // while(!q.empty()){
-        //     n--;
-        //     int node = q.front();
-        //     q.pop();
-        //     for(auto it : adj[node]){
-        //         if(--ind[it] == 0) q.push(it);
-        //     }
-        // }
-        // return n == 0;
+    bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int>adj[n];
-        int m = gn.size();
-        for(int i = 0; i<m; i++){
-            adj[gn[i][0]].push_back(gn[i][1]);
-        }
         vector<int>ind(n,0);
-        for(int i = 0; i<n; i++){
-            for(auto it : adj[i]){
-                ind[it]++;
-            }
+        for(int i = 0; i<pre.size(); i++){
+            adj[pre[i][0]].push_back(pre[i][1]);
+            ind[pre[i][1]]++;
         }
         queue<int>q;
         for(int i = 0; i<n; i++){
             if(ind[i] == 0) q.push(i);
         }
-        vector<int>ans;
         while(!q.empty()){
             int node = q.front();
-            ans.push_back(node);
             q.pop();
+            n--;
             for(auto it : adj[node]){
                 if(--ind[it] == 0) q.push(it);
             }
         }
-        return ans.size() == n;
+        return n == 0;
     }
 };

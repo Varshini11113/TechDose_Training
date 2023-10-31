ea2745e34596609fcabb00f91bdfd0aa2a01e443
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-10-31 11:18:55 +0530

    Time: 16 ms (74.28%) | Memory: 13.5 MB (91.46%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n];
+        vector<int>ind(n,0);
+        for(int i = 0; i<pre.size(); i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            ind[pre[i][0]]++;
+        }
+        queue<int>q;
+        vector<int>ans;
+        for(int i = 0; i<n; i++){
+            if(ind[i] == 0) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                if(--ind[it] == 0) q.push(it);
+            }
+        }
+        if(ans.size() == n) return ans;
+        else{
+            return vector<int>();
+        }
+    }
+};

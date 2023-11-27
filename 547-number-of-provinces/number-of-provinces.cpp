79971b6bdedbbf91e7ba664b62810df77436f305
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-11-28 01:01:10 +0530

    Time: 26 ms (33.62%) | Memory: 17 MB (32.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     void dfs(int node, vector<int>&vis, vector<int>adj[]){
         vis[node] = 1;
-        for(auto it: adj[node]){
+        for(auto it : adj[node]){
             if(!vis[it]){
                 dfs(it, vis, adj);
             }
@@ -16,6 +16,7 @@ public:
             for(int j = 0; j<n; j++){
                 if(isConnected[i][j] == 1 && i != j){
                     adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
@@ -23,8 +24,8 @@ public:
         int count = 0;
         for(int i = 0; i<n; i++){
             if(!vis[i]){
-                count++;
                 dfs(i, vis, adj);
+                count++;
             }
         }
         return count;

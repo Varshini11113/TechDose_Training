e3a2023e931a33ace2fa83afd1448af13e2f5ca8
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2023-12-06 07:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (70.53%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        // if(n <= 7){
+        //     return (n * (n-1)) / 2;
+        // }
+        int week_count = n / 7;
+        int remaining_days = n % 7;
+        
+        int total = ((week_count * (week_count - 1)) / 2) * 7;
+        total += week_count * 28; 
+        total += ((remaining_days * (remaining_days + 1)) / 2) + (week_count * remaining_days); 
+        
+        return total;
+    }
+};

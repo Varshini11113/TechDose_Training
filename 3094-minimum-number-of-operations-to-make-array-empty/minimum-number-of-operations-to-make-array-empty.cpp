f9c5d321fe595821f3a6e983af9f268a3a535afe
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-01-04 10:04:33 +0530

    Time: 131 ms (31.68%) | Memory: 85 MB (90.06%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -7,9 +7,12 @@ public:
             mp[i]++;
         }
         for(auto i : mp){
-            if(i.second < 2) return -1;
+            if(i.second == 1) return -1;
+            else if(i.second % 3 == 0){
+                ans += (i.second)/3;
+            }
             else{
-                ans += (i.second+2)/3;
+                ans+=(i.second/3)+1;
             }
         }
         return ans;

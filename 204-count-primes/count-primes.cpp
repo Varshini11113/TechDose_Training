769e2108f7ef21cce0be3b5fd2ee0688ed817bb6
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-01-09 05:34:31 +0530

    Time: 613 ms (57.41%) | Memory: 10.6 MB (83.69%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n <= 1) return 0;
+        // int count = 0;
+        vector<bool>isPrime(n,true);
+        isPrime[0] = false, isPrime[1] = false;
+        for(int i = 2; i<sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j = i*i; j<n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return count(isPrime.begin(), isPrime.end(), true);
+    }
+};

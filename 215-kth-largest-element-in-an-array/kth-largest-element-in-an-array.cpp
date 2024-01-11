a3d1126a8b32a8bd12eca37a98f5d0ed5e24574a
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-01-11 11:00:50 +0530

    Time: 91 ms (76.63%) | Memory: 55.5 MB (94.18%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,40 +1,7 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        return quickselect(nums, 0, n - 1, n - k);
-    }
-private:
-    int quickselect(vector<int>& nums, int left, int right, int k) {
-        if (left == right) {
-            return nums[left];
-        }
-
-        auto pivotIndex = left + (rand() % (right - left + 1));
-        auto [firstEqual, firstGreater] = threeWayPartition(nums, left, right, nums[pivotIndex]);
-        if (k < firstEqual) {
-            return quickselect(nums, left, firstEqual - 1, k);
-        }
-        if (k >= firstEqual && k < firstGreater) {
-            return nums[k];
-        }
-        return quickselect(nums, firstGreater, right, k);
-    }
-
-    pair<int, int> threeWayPartition(vector<int>& nums, int left, int right, int pivot) {
-        int i = left, j = left, k = right;
-        while (j <= k) {
-            if (nums[j] < pivot) {
-                swap(nums[i], nums[j]);
-                i++;
-                j++;
-            } else if (nums[j] == pivot) {
-                j++;
-            } else {
-                swap(nums[k], nums[j]);
-                k--;
-            }
-        }
-        return {i, j};
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
     }
 };

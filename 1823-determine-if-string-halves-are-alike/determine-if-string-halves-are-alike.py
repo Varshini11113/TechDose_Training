fb719ac0e20403dae99bcf78915545e9ff8d588f
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-01-24 20:28:05 +0530

    Time: 16 ms (80.70%) | Memory: 11.8 MB (99.28%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -1,21 +1,8 @@
 class Solution(object):
-    def isVowel(self, c):
-        return c.lower() in {'a', 'e', 'i', 'o', 'u'}
-
     def halvesAreAlike(self, s):
-        n = len(s)
-        count = 0
-
-        for i in range(n // 2):
-            if self.isVowel(s[i]):
-                count += 1
-
-        for i in range(n // 2, n):
-            if self.isVowel(s[i]):
-                count -= 1
-
-        return count == 0
-
-
-
-        
+        vowels="aeiouAEIOU"
+        l=len(s)
+        a=s[0:l//2]
+        b=s[l//2:l+1]
+        return sum(a.count(vowel) for vowel in vowels)==sum(b.count(vowel) for vowel in vowels)
+           

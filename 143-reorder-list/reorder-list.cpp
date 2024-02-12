8a8e2982a66dd9c8e6e6617c941e8160834caf68
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-02-13 00:22:02 +0530

    Time: 25 ms (63.68%) | Memory: 21.2 MB (27.55%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middle(ListNode* head){
+        ListNode* slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        } 
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL, *ext = NULL, *curr = head;
+        while(curr){
+            ext = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = ext;
+        } 
+        return prev;
+    }
+    void reorderList(ListNode* head) {
+        if(head==NULL or head->next==NULL){
+            return;
+        }
+        
+        ListNode*mid = middle(head);
+        ListNode*k = reverse(mid->next);
+        mid->next = NULL;
+        
+        ListNode*c1 = head;
+        ListNode*c2 = k;
+        ListNode*f1 = NULL;
+        ListNode*f2 = NULL;
+        
+        while(c1!=NULL and c2!=NULL)
+        {
+            f1 = c1->next;
+            f2 = c2->next;
+            
+            c1->next = c2;
+            c2->next = f1;
+            
+            c1 = f1;
+            c2 = f2;
+        }
+    }
+};

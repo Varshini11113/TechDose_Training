25b787a555d564e5b5a98e8f356013c36920b87f
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-02-14 12:48:46 +0530

    Time: 55 ms (46.34%) | Memory: 26.8 MB (42.35%) - LeetSync

--- 587-erect-the-fence/erect-the-fence.cpp
+++ 587-erect-the-fence/erect-the-fence.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    // Check if moving from point a to point b to point c results in a clockwise turn
-    bool isClockwiseTurn(vector<int> &a, vector<int> &b, vector<int> &c) {
+    bool isClockWise(vector<int>&a, vector<int>&b, vector<int>&c){
         int x1 = a[0], x2 = b[0], x3 = c[0];
         int y1 = a[1], y2 = b[1], y3 = c[1];
         return ((y3 - y2) * (x2 - x1)) - ((y2 - y1) * (x3 - x2)) >= 0;
     }
-    
-    // Find the convex hull of a set of points
-    vector<vector<int>> outerTrees(vector<vector<int>>& points) {
-        vector<vector<int>> hull;
-        int n = points.size();
-       
-        // Comparator to sort points based on their x-coordinate and then y-coordinate
-        auto comparator = [&](vector<int> &a, vector<int> &b) {
-            if (a[0] != b[0])
+
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        vector<vector<int>>hull;
+        int n = trees.size();
+        auto comparator = [&](vector<int>&a, vector<int>&b){
+            if(a[0] != b[0]){
                 return a[0] < b[0];
+            }
             return a[1] < b[1];
         };
-        
-        sort(points.begin(), points.end(), comparator);
-        
-        // Build the upper part of the convex hull
-        for(int i = 0; i < n; i++) {
-            while(hull.size() > 1 && !isClockwiseTurn(hull[hull.size() - 2], hull[hull.size() - 1], points[i])) {
+        sort(trees.begin(), trees.end(), comparator);
+
+        for(int i = 0; i<n; i++){
+            while(hull.size() > 1 && !isClockWise(hull[hull.size()-2], hull[hull.size()-1], trees[i])){
                 hull.pop_back();
             }
-            hull.push_back(points[i]);
+            hull.push_back(trees[i]);
         }
-        
-        // Build the lower part of the convex hull
-        for(int i = n - 2; i >= 0; i--) {
-            while(hull.size() > 1 && !isClockwiseTurn(hull[hull.size() - 2], hull[hull.size() - 1], points[i])) {
+
+        for(int i = n-2; i>=0; i--){
+            while(hull.size() > 1 && !isClockWise(hull[hull.size()-2], hull[hull.size()-1], trees[i])){
                 hull.pop_back();
             }
-            hull.push_back(points[i]);
+            hull.push_back(trees[i]);
         }
-        
-        set<vector<int>> uniquePoints(hull.begin(), hull.end());
-        hull.assign(uniquePoints.begin(), uniquePoints.end());
+
+        set<vector<int>> uniquePts(hull.begin(), hull.end());
+        hull.assign(uniquePts.begin(), uniquePts.end());
         return hull;
     }
 };

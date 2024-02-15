05c57079eb3d4072e60313a02a56ed93786a3019
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-02-15 15:06:10 +0530

    Time: 31 ms (39.11%) | Memory: 12 MB (63.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:    
-    int dp[1001][1001] = {-1};
+    int dp[1001][1001];
     int LCS(string& s1, string& s2, int n, int m)
     {
         if (n == 0 || m == 0)
             return 0;
-        if (dp[n][m] != -1)
+        if(dp[n][m] != -1){
             return dp[n][m];
+        }
         if (s1[n - 1] == s2[m - 1])
             return dp[n][m] = 1 + LCS(s1, s2, n - 1, m - 1);
-        else
-            return dp[n][m] = max(0 + LCS(s1, s2, n - 0, m - 1), 0 + LCS(s1, s2, n - 1, m - 0));
+        else{
+            int max_s1 = 0 + LCS(s1, s2, n - 0, m - 1);
+            int max_s2 = 0 + LCS(s1, s2, n - 1, m - 0);
+            return dp[n][m] = max(max_s1, max_s2);
+        }
     }
     int longestCommonSubsequence(string s1, string s2) 
     {

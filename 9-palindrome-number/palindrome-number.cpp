988ae1136cb6e7c5ddc99d24ac11e0cc8666f91e
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-03-17 23:47:57 +0530

    Time: 44 ms (5.05%) | Memory: 8.1 MB (75.69%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    long long int reverse(int x){
-        long long int n = 0;
-        while(x > 0){
-            // x /= 10;
-            n = (n * 10) + (x%10);
-            cout << n << " ";
-            x /= 10;
-        }
-        return n;
-    }
     bool isPalindrome(int x) {
-        if( x < 0 ) return false;
-        long long int n = reverse(x);
-        return n == x;
+        int newe = x;
+        long long n = 0;
+        while(x > 0){
+            n = (n * 10) + (x % 10);
+            cout << n << " ";
+            x/=10;
+        }
+        return n == newe;
     }
 };

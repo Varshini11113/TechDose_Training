de92a507dc720050fa3145e1be2d3b015cdc6693
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-03-25 05:57:55 +0530

    Time: 30 ms (95.98%) | Memory: 35.9 MB (90.94%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -4,10 +4,11 @@ public:
         vector<int>res;
         int n = nums.size();
         for(int i = 0; i<n; i++){
-            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
-            if(nums[abs(nums[i]) - 1] > 0){
+            int ind = abs(nums[i]) - 1;
+            if(nums[ind] < 0){
                 res.push_back(abs(nums[i]));
             }
+            nums[ind] = nums[ind]*-1;
         }
         return res;
     }

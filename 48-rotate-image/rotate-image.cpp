3309ae0c2d6bd082adfba157ccae71579e82ddfd
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-04-01 09:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (21.64%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>>res(n, vector<int>(m,0));
+        int n = matrix.size();
+        // int n = matrix[0].size();
+        // vector<vector<int>>res(n, vector<int>(m,0));
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                res[i][j] = matrix[j][i];
+            for(int j = i; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         cout << res[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                cout << res[i][j] << " ";
-            }
-            cout << endl;
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-        for(int i = 0; i<n; i++){
-            reverse(res[i].begin(), res[i].end());
-        }
-        matrix = res;
+        // matrix = res;
     }   
 };

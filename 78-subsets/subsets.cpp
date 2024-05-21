ef0c6009b41adbc54ee176bcce5ae37f27641822
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-05-21 18:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.15%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    void findsubsets(vector<int>&nums, int i, vector<vector<int>>&ans, vector<int>&ds, int n){
-        // int n = nums.size();
-        if(i == n){
-            ans.push_back(ds);
-            return;
+
+        void subs(vector<int>& nums , vector<vector<int>>& finans , vector<int>& ans ,int i , int n){
+
+            if(i >= n){
+                finans.push_back(ans);
+                return;
+            }
+
+            ans.push_back(nums[i]);
+            subs(nums , finans , ans , i+1 , n);
+            ans.pop_back();
+            subs(nums , finans , ans , i+1 , n );
+
+
         }
-        ds.push_back(nums[i]);
-        findsubsets(nums, i+1, ans, ds, n);
-        ds.pop_back();
-        findsubsets(nums, i+1, ans, ds, n);
-    }
+
+
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>>ans;
-       vector<int>ds;
-       int n = nums.size();
-       findsubsets(nums, 0, ans, ds, n);
-       return ans; 
+
+        vector<vector<int>> finans;
+        vector<int> ans;
+        int n = nums.size();
+        int i = 0;
+
+        subs(nums , finans , ans , i , n);
+        return finans;
+
+        
     }
 };

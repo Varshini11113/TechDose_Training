dba1071b1283909f951e9f677eced5b73dc1bc25
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-06-27 12:03:10 +0530

    Time: 4 ms (37.84%) | Memory: 8.1 MB (93.10%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void subsequence(int i, vector<int>&ele, vector<int>&nums, int n, vector<vector<int>>&ds){
-        // vector<int>ele;
+    void subsequence(int i, vector<int>&ele, vector<int>&nums, vector<vector<int>>&ds, int n){
         if(i == n){
             ds.push_back(ele);
             return;
         }
         ele.push_back(nums[i]);
-        subsequence(i+1, ele, nums, n, ds);
+        subsequence(i+1, ele, nums, ds, n);
         ele.pop_back();
-        subsequence(i+1, ele, nums, n, ds);
+        subsequence(i+1, ele, nums, ds, n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ds;
-        vector<int>ele;
         int n = nums.size();
-        subsequence(0, ele, nums, n, ds);
+        vector<int>ele;
+        vector<vector<int>>ds;
+        subsequence(0, ele, nums, ds, n);
         return ds;
     }
 };

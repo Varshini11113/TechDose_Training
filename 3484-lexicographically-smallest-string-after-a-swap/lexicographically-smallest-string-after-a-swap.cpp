94bc44ba57ee684c7c2523cc94145195d2f05acf
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-07-16 05:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (54.99%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -3,7 +3,7 @@ public:
     string getSmallestString(string s) {
         int n = s.length();
         for(int i = 0; i<n-1; i++){
-            if((s[i] % 2 == 0 && s[i+1] % 2 == 0) || (s[i] % 2 != 0 && s[i+1] % 2 != 0)){
+            if(s[i] % 2 == s[i+1] % 2){
                 if(s[i] > s[i+1]){
                     char temp = s[i];
                     s[i] = s[i+1];
